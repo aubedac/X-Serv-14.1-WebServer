a97088bad43e2bec15572d4d785e4a9074b8f5e3
Adrián Úbeda <adrianubedaportugues@gmail.com>
2016-02-15 13:21:42 +0100

    Ejercicio realizado.

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -28,9 +28,13 @@ mySocket.listen(5)
 while True:
     print 'Waiting for connections'
     (recvSocket, address) = mySocket.accept()
+    #print address
+    IP = address[0]
+    Puerto = str(address[1]) 
     print 'HTTP request received:'
     print recvSocket.recv(1024)
     recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
+                    "<html><body><h1>Hola! eres esta IP (" + IP + ")" +
+                    " y de este Puerto (" + Puerto + ")" + "</h1></body></html>" +
                     "\r\n")
     recvSocket.close()
